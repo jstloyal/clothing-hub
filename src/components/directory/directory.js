792b5abf555736c3cb99b96cb705be3e3ff48976
jstloyal <jstloyalty@gmail.com>
2021-01-04 23:44:12 +0100

    finish styling menu item

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -10,28 +10,28 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'LAPTOP',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl: 'https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bGFwdG9wfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
           id: 1,
         },
         {
           title: 'DESKTOP',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://images.unsplash.com/photo-1593640495253-23196b27a87f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGRlc2t0b3B8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
           id: 2,
         },
         {
           title: 'ACCESSORIES',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://hips.hearstapps.com/bpc.h-cdn.co/assets/17/30/1501000275-iphone-accessories.jpg',
           id: 3,
         },
         {
           title: 'APPLE',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://images.macrumors.com/t/lts0uH8gcDfjexvr-SHPBQIVQW8=/1600x900/smart/article-new/2018/07/appleproductlineup.jpg',
           size: 'large',
           id: 4,
         },
         {
           title: 'PC',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://www.ultrabookreview.com/wp-content/uploads/2016/06/smudgy-blade-clean-xps.jpg',
           size: 'large',
           id: 5,
         }
@@ -42,8 +42,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     )
