f6b6ff9d278954bf4866d0956782176fb66c63f2
jstloyal <jstloyalty@gmail.com>
2021-01-04 18:00:44 +0100

    add homepage component and sass package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <HomePage />
     </div>
   );
 }
