73ddc05b660da601dae642ad1ea836e43fd742dd
jstloyal <jstloyalty@gmail.com>
2021-01-05 10:34:51 +0100

    add react-router-dom and setup basic navigation inside menu-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 
+const LaptopsPage = () => (
+  <div><h1>LAPTOPS PAGE</h1></div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/laptops' component={LaptopsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -12,28 +12,33 @@ class Directory extends React.Component {
           title: 'LAPTOP',
           imageUrl: 'https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bGFwdG9wfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
           id: 1,
+          linkUrl: 'laptops'
         },
         {
           title: 'DESKTOP',
-          imageUrl: 'https://images.unsplash.com/photo-1593640495253-23196b27a87f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGRlc2t0b3B8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+          imageUrl: 'https://storage-asset.msi.com/event/spb/2019/best-gaming-desktop-pc/images/kv-pd.png',
           id: 2,
+          linkUrl: ''
         },
         {
           title: 'ACCESSORIES',
           imageUrl: 'https://hips.hearstapps.com/bpc.h-cdn.co/assets/17/30/1501000275-iphone-accessories.jpg',
           id: 3,
+          linkUrl: ''
         },
         {
           title: 'APPLE',
-          imageUrl: 'https://images.macrumors.com/t/lts0uH8gcDfjexvr-SHPBQIVQW8=/1600x900/smart/article-new/2018/07/appleproductlineup.jpg',
+          imageUrl: 'https://images.macrumors.com/t/u5qFUnuK3qopG8nIsOOX74kgtk8=/1600x0/article-new/2019/02/MR-Future-Products-2020-2.png',
           size: 'large',
           id: 4,
+          linkUrl: ''
         },
         {
           title: 'PC',
-          imageUrl: 'https://www.ultrabookreview.com/wp-content/uploads/2016/06/smudgy-blade-clean-xps.jpg',
+          imageUrl: 'https://cdn.mos.cms.futurecdn.net/vEcELHdn998wRTcCzqV5m9.jpg',
           size: 'large',
           id: 5,
+          linkUrl: ''
         }
       ]
     }
@@ -42,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherProps  }) => (
+          <MenuItem key={id} {...otherProps} />
         ))}
       </div>
     )
