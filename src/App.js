6d3228e90baf93bbb80c9507fa18ebe32a92b3fb
jstloyal <jstloyalty@gmail.com>
2021-01-06 10:56:29 +0100

    add signin page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/Shop.jsx';
 import Header from './components/header/header.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in';
+import './sign-in-and-sign-up.styles.scss';
+
+function SignInAndSignUp() {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInAndSignUp;
