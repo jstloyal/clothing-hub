9f3f753309e0d314befee5d283020d22a5791e8a
jstloyal <jstloyalty@gmail.com>
2021-01-10 14:06:43 +0100

    integrated firebase into application, add sign in component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-function Header() {
+function Header({ currentUser }) {
   return (
     <div className='header'>
       <Link to="/" className='logo-container'>
@@ -16,6 +17,11 @@ function Header() {
         <Link to='/shop' className='option'>
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        ) : (
+          <Link className='option' to='/signIn'>SIGN IN</Link>
+        )}
       </div>
     </div>
   )
