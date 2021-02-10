cb97fa83cde7122e344d2419c3ffeadd02c8c13f
jstloyal <jstloyalty@gmail.com>
2021-02-10 03:40:44 +0100

    Data normalization and update collection page

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collectionsOverview'; 
+import CollectionPage from '../collection/Collection';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
